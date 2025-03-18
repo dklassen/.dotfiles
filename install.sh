1db2cddd8ab37041ef08090a768e159a6ef70e70
Dana Klassen <dana.klassen@noibu.com>
2025-03-18 09:35:39 -0400

    Add notes, brew packages, and tigrc

--- install.sh
+++ install.sh
@@ -1,9 +1,46 @@
 #!/usr/bin/env bash
 
-set -eo pipefail 
+set -euo pipefail
 
+function echo_info() {
+    echo -e "\033[1;34m[INFO]\033[0m $1"
+}
+
+function echo_error() {
+    echo -e "\033[1;31m[ERROR]\033[0m $1" >&2
+}
+
+install_brew_package() {
+    local package=$1
+    if ! brew list "$package" &>/dev/null; then
+        echo_info "Installing $package..."
+        brew install "$package"
+    else
+        echo_info "$package is already installed. Skipping."
+    fi
+}
+
+#####################################
+# Homebrew setup and install base 
+# packages.
+# ####################################
+if ! command -v brew &>/dev/null; then
+    echo_info "Homebrew not found. Installing..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo_info "Homebrew is already installed. Skipping installation."
+fi
+
+eval "$(/opt/homebrew/bin/brew shellenv)" 2>/dev/null || true
+
+brew update
+
+install_brew_package tig
+install_brew_package ripgrep
+install_brew_package fd
 
 echo "Installing neovim files"
 cp -R nvim/  ~/.config/nvim
+
 
 
--- nvim/lua/plugins/basic.lua
+++ nvim/lua/plugins/basic.lua
@@ -6,6 +6,37 @@ return {
 		lazy = true,
 	},
 	{
+	    'gsuuon/note.nvim',
+	    opts = {
+	      -- opts.spaces are note workspace parent directories.
+	      -- These directories contain a `notes` directory which will be created if missing.
+	      -- `<space path>/notes` acts as the note root, so for space '~' the note root is `~/notes`.
+	      -- Defaults to { '~' }.
+	      spaces = {
+		'~',
+		-- '~/projects/foo'
+	      },
+
+	      -- Set keymap = false to disable keymapping
+	      -- keymap = { 
+	      --   prefix = '<leader>n'
+	      -- }
+	    },
+	    cmd = 'Note',
+	    ft = 'note',
+	    keys = {
+	      -- You can use telescope to search the current note space:
+	      {'<leader>tn', -- [t]elescope [n]ote
+		function()
+		  require('telescope.builtin').live_grep({
+		    cwd = require('note.api').current_note_root()
+		  })
+		end,
+		mode = 'n'
+	      }
+	    }
+	  },		
+ 	  {
 		'folke/tokyonight.nvim',
 		lazy = false,
 		priority = 1000,
